afa03417ee05768acca3b35d0737804b1b7df529
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-04-29 14:14:37 -0400

    Adding analysis xml import

---
+++ metadataFunctions/batonMetadataIngest.py
@@ -0,0 +1,21 @@
+#!/opt/cantemo/python/bin/python
+
+# This application ingests metadata from Baton XML output file into Cantemo
+# PREREQUISITE: -none-
+# 	Usage: batonMetadataIngest.py [full file path of the XML file
+
+#------------------------------
+# Libraries
+import os
+import glob
+import sys
+import datetime
+import time
+import subprocess
+import xml.etree.ElementTree as ET
+#------------------------------
+
+tree = ET.parse('faderstatic/atsCodes/sampleFiles/Baton/Grand_HD_RU_SGRAND1_S5E1_Master_mxf.xml')
+root = tree.getroot()
+
+print(root.tag)
