d59cf6679f38a295cb4240a9853b94d53231699f
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-04-30 14:59:09 -0400

    modify curl commands

--- olympusatWorkflow/episodeWorkflow-createSeriesAndSeasonPlaceholders_v1.0.sh
+++ olympusatWorkflow/episodeWorkflow-createSeriesAndSeasonPlaceholders_v1.0.sh
@@ -62,7 +62,7 @@ else
         itemLicensor=$(filterVidispineItemMetadata $itemId "metadata" "oly_licensor")
         #seriesCreateHttpResponse=$(curl -X POST "http://10.1.1.34/API/v2/items/" -H "accept: application/json" -H "Content-Type: application/json" -H "X-CSRFToken: u3qcM502KaqTlwp9rDT0dqZw3cePwHN9rLaCojxekvUuYeKgi5ikanKUoe5zziQL" -d "{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"Test Series API\" }, { \"name\": \"oly_contentType\", \"value\": \"series\" }, { \"name\": \"oly_licensor\", \"value\": \"olympusat\" } ] }}")
         seriesCreateBody="{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"$checkForSeriesName\" }, { \"name\": \"oly_contentType\", \"value\": \"series\" }, { \"name\": \"oly_licensor\", \"value\": \"$itemLicensor\" } ] }}"
-        seriesCreateHttpResponse=$(curl -X POST $createUrl -H "accept: application/json" -H "Content-Type: application/json" -H "X-CSRFToken: u3qcM502KaqTlwp9rDT0dqZw3cePwHN9rLaCojxekvUuYeKgi5ikanKUoe5zziQL" -d $seriesCreateBody)
+        seriesCreateHttpResponse=$(curl --location --request POST $createUrl --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CRbBvVEFSfR5lHoQebsbQemRRas2MUyo53CsO5ixtkSrzvC9H7NffcuaXkIJvr1V' --data $seriesCreateBody)
         echo "Series Create httpResponse - [$seriesCreateHttpResponse]"
     fi
 
@@ -79,7 +79,7 @@ else
         itemLicensor=$(filterVidispineItemMetadata $itemId "metadata" "oly_licensor")
         #seriesCreateHttpResponse=$(curl -X POST "http://10.1.1.34/API/v2/items/" -H "accept: application/json" -H "Content-Type: application/json" -H "X-CSRFToken: u3qcM502KaqTlwp9rDT0dqZw3cePwHN9rLaCojxekvUuYeKgi5ikanKUoe5zziQL" -d "{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"Test Season API\" }, { \"name\": \"oly_contentType\", \"value\": \"season\" }, { \"name\": \"oly_licensor\", \"value\": \"olympusat\" }, { \"name\": \"oly_seasonNumber\", \"value\": \"1\" } ] }}")
         seasonCreateBody="{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"$checkForSeasonName\" }, { \"name\": \"oly_contentType\", \"value\": \"season\" }, { \"name\": \"oly_licensor\", \"value\": \"$itemLicensor\" }, { \"name\": \"oly_seasonNumber\", \"value\": \"$itemSeasonNumber\" } ] }}"
-        seasonCreateHttpResponse=$(curl -X POST $createUrl -H "accept: application/json" -H "Content-Type: application/json" -H "X-CSRFToken: u3qcM502KaqTlwp9rDT0dqZw3cePwHN9rLaCojxekvUuYeKgi5ikanKUoe5zziQL" -d $seasonCreateBody)
+        seasonCreateHttpResponse=$(curl --location --request POST $createUrl --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CRbBvVEFSfR5lHoQebsbQemRRas2MUyo53CsO5ixtkSrzvC9H7NffcuaXkIJvr1V' --data $seasonCreateBody)
         echo "Season Create httpResponse - [$seasonCreateHttpResponse]"
     fi
 fi
