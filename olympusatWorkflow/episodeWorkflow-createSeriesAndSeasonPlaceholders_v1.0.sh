8930953cf1c10e5b6cf6319dfebb8d1a7eb8c3a8
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-05-01 07:25:52 -0400

    cleaning up and adding logging

--- olympusatWorkflow/episodeWorkflow-createSeriesAndSeasonPlaceholders_v1.0.sh
+++ olympusatWorkflow/episodeWorkflow-createSeriesAndSeasonPlaceholders_v1.0.sh
@@ -20,7 +20,7 @@ IFS=$(echo -e "\n\b")
 export mydate=$(date +%Y-%m-%d)
 export datetime=$(date +%Y/%m/%d_%H:%M)
 
-logfile="/opt/olympusat/logs/qcWorkflow-$mydate.log"
+logfile="/opt/olympusat/logs/olympusatWorkflow-$mydate.log"
 
 #Set Variable to check before continuing with script
 export itemId=$1
@@ -30,57 +30,74 @@ echo "Content Type - [$itemContentType]"
 #Check Variable
 if [[ "$itemContentType" != "episode" ]];
 then
-    echo "contentType is NOT 'episode'-skip process"
+    #echo "contentType is NOT 'episode'-skip process"
+    
     #contentType is NOT 'episode'-skip process
     echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> $logfile
     echo "$datetime - (episodeWorkflow) - Item ID - $itemId" >> "$logfile"
     echo "$datetime - (episodeWorkflow) - Content Type is NOT 'episode'" >> "$logfile"
+    echo "$datetime - (episodeWorkflow) - Content Type is [$itemContentType]" >> "$logfile"
     echo "$datetime - (episodeWorkflow) - Skipping Episode Workflow" >> "$logfile"
 else
-    echo "contentType IS episode-continue with process"
+    #echo "contentType IS episode-continue with process"
+    
     #contentType IS episode-continue with process
     #Variables to be passed from Cantemo to shell script
+
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> $logfile
+    echo "$datetime - (episodeWorkflow) - Item ID - $itemId" >> "$logfile"
+    echo "$datetime - (episodeWorkflow) - Content Type is [$itemContentType]" >> "$logfile"
+
     itemSeriesName=$(filterVidispineItemMetadata $itemId "metadata" "oly_seriesName")
     itemSeasonNumber=$(filterVidispineItemMetadata $itemId "metadata" "oly_seasonNumber")
     checkForSeriesItem="$itemSeriesName"
     checkForSeasonItem="$itemSeriesName | Season $itemSeasonNumber"
 
-    echo "Check for Series Item - [$checkForSeriesItem]"
-    echo "Check for Season Item - [$checkForSeasonItem]"
-
     export searchUrl="http://10.1.1.34/API/v2/search/"
     export createUrl="http://10.1.1.34/API/v2/items/"
 
     #API Call to Search if Series already exists
-    #seriesHttpResponse=$(curl --location --request PUT 'http://10.1.1.34/API/v2/search/' --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=VDa9RP3Y9rgomyzNWvRxbu7WdTMetVYBlLg6pGMIJ6oyVABsjJiiEK9JCWVA1HYd' --data '{"filter": {"operator": "AND","terms": [{"name": "title", "value": "Khloe"},{"name": "oly_contentType", "value": "series"}]}}')
+
+    echo "$datetime - (episodeWorkflow) - Checking if Series item exists - [$checkForSeriesItem]" >> "$logfile"
+
     seriesCheckBody="{ \"filter\": { \"operator\": \"AND\",\"terms\": [{ \"name\": \"title\", \"value\": \"$checkForSeriesItem\" },{ \"name\": \"oly_contentType\", \"value\": \"series\" }]}}"
     seriesCheckHttpResponse=$(curl --location --request PUT $searchUrl --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=VDa9RP3Y9rgomyzNWvRxbu7WdTMetVYBlLg6pGMIJ6oyVABsjJiiEK9JCWVA1HYd' --data $seriesCheckBody)
-    echo "Series Search httpResponse - [$seriesCheckHttpResponse]"
 
     if [[ "$seriesCheckHttpResponse" != *""id":"OLY-""* ]];
     then
+        #Series placeholder does not exists, API Call to create new Series placeholder with metadata
+
+        echo "$datetime - (episodeWorkflow) - Creating new Series placeholder - [$checkForSeriesItem]" >> "$logfile"
+
         itemLicensor=$(filterVidispineItemMetadata $itemId "metadata" "oly_licensor")
-        #seriesCreateHttpResponse=$(curl -X POST "http://10.1.1.34/API/v2/items/" -H "accept: application/json" -H "Content-Type: application/json" -H "X-CSRFToken: u3qcM502KaqTlwp9rDT0dqZw3cePwHN9rLaCojxekvUuYeKgi5ikanKUoe5zziQL" -d "{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"Test Series API\" }, { \"name\": \"oly_contentType\", \"value\": \"series\" }, { \"name\": \"oly_licensor\", \"value\": \"olympusat\" } ] }}")
         seriesCreateBody="{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"$checkForSeriesItem\" }, { \"name\": \"oly_contentType\", \"value\": \"series\" }, { \"name\": \"oly_licensor\", \"value\": \"$itemLicensor\" } ] }}"
         seriesCreateHttpResponse=$(curl --location --request POST $createUrl --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CRbBvVEFSfR5lHoQebsbQemRRas2MUyo53CsO5ixtkSrzvC9H7NffcuaXkIJvr1V' --data $seriesCreateBody)
-        echo "Series Create httpResponse - [$seriesCreateHttpResponse]"
+    else
+        #Series placeholder already exists
+        echo "$datetime - (episodeWorkflow) - Series placeholder already exists - [$checkForSeriesItem]" >> "$logfile"
     fi
 
     sleep 2
 
-    #API Call to Search if Series already exists
-    #seasonHttpResponse=$(curl --location --request PUT 'http://10.1.1.34/API/v2/search/' --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=VDa9RP3Y9rgomyzNWvRxbu7WdTMetVYBlLg6pGMIJ6oyVABsjJiiEK9JCWVA1HYd' --data '{"filter": {"operator": "AND","terms": [{"name": "title", "value": "Khloe | Season 1"},{"name": "oly_contentType", "value": "season"}]}}')
+    #API Call to Search if Season already exists
+
+    echo "$datetime - (episodeWorkflow) - Checking if Season item exists - [$checkForSeasonItem]" >> "$logfile"
+
     seasonBody="{ \"filter\": { \"operator\": \"AND\",\"terms\": [{ \"name\": \"title\", \"value\": \"$checkForSeasonItem\" },{ \"name\": \"oly_contentType\", \"value\": \"season\" }]}}"
     seasonHttpResponse=$(curl --location --request PUT $searchUrl --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=VDa9RP3Y9rgomyzNWvRxbu7WdTMetVYBlLg6pGMIJ6oyVABsjJiiEK9JCWVA1HYd' --data $seasonBody)
-    echo "Season Search httpResponse - [$seasonHttpResponse]"
 
     if [[ "$seasonCheckHttpResponse" != *""id":"OLY-""* ]];
     then
+        #Season placeholder does not exist, API Call to create new Season placeholder with metadata
+
+        echo "$datetime - (episodeWorkflow) - Creating new Season placeholder - [$checkForSeasonItem]" >> "$logfile"
+
         itemLicensor=$(filterVidispineItemMetadata $itemId "metadata" "oly_licensor")
-        #seriesCreateHttpResponse=$(curl -X POST "http://10.1.1.34/API/v2/items/" -H "accept: application/json" -H "Content-Type: application/json" -H "X-CSRFToken: u3qcM502KaqTlwp9rDT0dqZw3cePwHN9rLaCojxekvUuYeKgi5ikanKUoe5zziQL" -d "{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"Test Season API\" }, { \"name\": \"oly_contentType\", \"value\": \"season\" }, { \"name\": \"oly_licensor\", \"value\": \"olympusat\" }, { \"name\": \"oly_seasonNumber\", \"value\": \"1\" } ] }}")
         seasonCreateBody="{ \"metadata\": { \"group_name\": \"Olympusat\", \"fields\": [ { \"name\": \"title\", \"value\": \"$checkForSeasonItem\" }, { \"name\": \"oly_contentType\", \"value\": \"season\" }, { \"name\": \"oly_licensor\", \"value\": \"$itemLicensor\" }, { \"name\": \"oly_seasonNumber\", \"value\": \"$itemSeasonNumber\" }, { \"name\": \"oly_seriesName\", \"value\": \"$checkForSeriesItem\" } ] }}"
         seasonCreateHttpResponse=$(curl --location --request POST $createUrl --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CRbBvVEFSfR5lHoQebsbQemRRas2MUyo53CsO5ixtkSrzvC9H7NffcuaXkIJvr1V' --data $seasonCreateBody)
-        echo "Season Create httpResponse - [$seasonCreateHttpResponse]"
+    else
+        #Season placeholder already exists
+        echo "$datetime - (episodeWorkflow) - Season placeholder already exists - [$checkForSeriesItem]" >> "$logfile"
     fi
 fi
 
