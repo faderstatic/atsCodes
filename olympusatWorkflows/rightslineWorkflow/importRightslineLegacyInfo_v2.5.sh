7911600a068d0cc12d4f8304181e726f2a377232
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-05-02 12:55:00 -0400

    changed field name and value to check if already processed

--- olympusatWorkflows/rightslineWorkflow/importRightslineLegacyInfo_v2.5.sh
+++ olympusatWorkflows/rightslineWorkflow/importRightslineLegacyInfo_v2.5.sh
@@ -96,10 +96,10 @@ export cantemoItemTitle=$(filterVidispineItemMetadata "$cantemoItemId" "metadata
 
 # --------------------------------------------------
 # Check to see if import has already ran on item
-urlGetItemInfo="http://10.1.1.34:8080/API/item/$itemId/metadata?field=oly_adminRulesFlags&terse=yes"
+urlGetItemInfo="http://10.1.1.34:8080/API/item/$itemId/metadata?field=oly_rightslineInfo&terse=yes"
 httpResponse=$(curl --location --request GET $urlGetItemInfo --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=Tkb9vkSC8v4SceB8CHUyB3iaMPjvgoHrzhLrvo36agG3wqv0jHc7nsOtdTo9JEyM')
 
-if [[ "$httpResponse" != *"rightslineinfoimported"* ]];
+if [[ "$httpResponse" != *"metadataimported"* ]];
 then
     partialRow="false"
     lineReadComplete="false"
@@ -380,7 +380,7 @@ then
 
         sleep 2
 
-        updateVidispineMetadata $itemId "oly_adminRulesFlags" "rightslineinfoimported"
+        updateVidispineMetadata $cantemoItemId "oly_rightslineInfo" "metadataimported"
         
         sleep 2
 
