dd8e90d6ccee76010e2d2312e836b225ffbf930f
faderstatic <tang26.2@gmail.com>
2024-05-03 14:29:27 -0400

    Adding Python Script requestFirstAiredDate

--- metadataFunctions/requestFirstAiredDate.py
+++ metadataFunctions/requestFirstAiredDate.py
@@ -15,76 +15,80 @@ import time
 import subprocess
 import requests
 import xml.etree.ElementTree as ET
+import json
+from requests.exceptions import HTTPError
 #------------------------------
 
-cantemoItemId = sys.argv[1]
-# cantemoItemId = os.environ.get("portal_itemId")
+try:
+  cantemoItemId = sys.argv[1]
+  # cantemoItemId = os.environ.get("portal_itemId")
 
-#------------------------------
-# Making API call to Cantemo to get contract ID
-headersCantemo = {
-  'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
-  'Cookie': 'csrftoken=HFOqrbk9cGt3qnc6WBIxWPjvCFX0udBdbJnzCv9jECumOjfyG7SS2lgVbFcaHBCc'
-}
-payloadCantemo = {}
-urlGetContractId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineContractId&terse=yes"
-httpApiResponse = requests.request("GET", urlGetContractId, headers=headersCantemo, data=payloadCantemo)
-<<<<<<< HEAD
-=======
-print(httpApiResponse.text)
->>>>>>> 9821b1d0e7a02f48af383007dddf538378ff1f8e
-#------------------------------
+  #------------------------------
+  # Making API call to Cantemo to get contract ID
+  headersCantemo = {
+    'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
+    'Cookie': 'csrftoken=HFOqrbk9cGt3qnc6WBIxWPjvCFX0udBdbJnzCv9jECumOjfyG7SS2lgVbFcaHBCc'
+  }
+  payloadCantemo = {}
+  urlGetContractId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineContractId&terse=yes"
+  httpApiResponse = requests.request("GET", urlGetContractId, headers=headersCantemo, data=payloadCantemo)
+  httpApiResponse.raise_for_status()
+  #------------------------------
 
-#------------------------------
-# Parsing XML data
-<<<<<<< HEAD
-ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
-responseXml = httpApiResponse.text
-print(responseXml)
-responseXmlRoot = ET.fromstring(responseXml)
-# print(responseXmlRoot.tag, responseXmlRoot.attrib)
-cantemoContractId = responseXmlRoot
-for child in cantemoContractId:
-    print(child.tag)
-# print(cantemoContractId.attrib)
-=======
-# ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
-# responseXml = httpApiResponse.text
-# responseXmlRoot = ET.fromstring(responseXml)
-# fileLocation = responseXmlRoot.find('{http://xml.vidispine.com/schema/vidispine}uri')
->>>>>>> 9821b1d0e7a02f48af383007dddf538378ff1f8e
-#------------------------------
+  #------------------------------
+  # Parsing XML data to get Rightsline contract ID (rightslineContrackId)
+  ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
+  responseXml = httpApiResponse.text
+  root = ET.XML(responseXml)
+  ns = {'vidispine': 'http://xml.vidispine.com/schema/vidispine'}
+  # print(root.tag, root.attrib, root.text)
+  for child in root.findall('vidispine:item', ns):
+    for cantemoContractId in child.findall('oly_rightslineContractId'):
+      rightslineContractId = cantemoContractId.text
+  #------------------------------
 
-#------------------------------
-# Formatting sorce  filename
-# baseFileName = os.path.basename(fileLocation.text)
-# justFileName, justFileExtension = os.path.splitext(baseFileName)
-# justFileExtensionTrimmed = justFileExtension.replace('.', '')
-# print(f"Filename: {justFileName} - File Extension: {justFileExtension}")
-# sourceXmlFile = f"/Volumes/creative/MAM/_autoIngest/staging/zAdmin/xmlImport/{justFileName}_{justFileExtensionTrimmed}.xml"
-# sourceXmlFile = f"/mnt/c/Users/kkanjanapitak/Desktop/Repositories/atsCodes/sampleFiles/Baton/Grand_HD_RU_SGRAND1_S5E1_Master_mxf.xml"
-#------------------------------
+   #------------------------------
+  # Making API call to Cantemo to get Rightsline Item ID
+  urlGetRightslineItemId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineItemId&terse=yes"
+  httpApiResponse = requests.request("GET", urlGetRightslineItemId, headers=headersCantemo, data=payloadCantemo)
+  httpApiResponse.raise_for_status()
+  #------------------------------
 
-# tree = ET.parse(sourceXmlFile)
-# root = tree.getroot()
+  #------------------------------
+  # Parsing XML data to get Rightsline contract ID (rightslineContrackId)
+  ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
+  responseXml = httpApiResponse.text
+  root = ET.XML(responseXml)
+  ns = {'vidispine': 'http://xml.vidispine.com/schema/vidispine'}
+  # print(root.tag, root.attrib, root.text)
+  for child in root.findall('vidispine:item', ns):
+    for cantemoRightslineId in child.findall('oly_rightslineItemId'):
+      rightslineItemId = cantemoRightslineId.text
+  #------------------------------
+  
+  #------------------------------
+  # Making API call to Mira to get First Aired Date
+  headersMira = {
+    'Cotent-Type': 'text/plain'
+  }
+  payloadMira = {}
+  rightslineContractId = 'C000000' # this is only for testing
+  rightslineItemId = 'MMFQEM1' # this is only for testing
+  urlGetFAD = f"http://10.1.1.22:83/Service1.svc/first_aired_date/{rightslineContractId}-{rightslineItemId}"
+  httpFADResponse = requests.request("GET", urlGetFAD, headers=headersMira, data=payloadMira)
+  httpFADResponse.raise_for_status()
+  responseJson = httpFADResponse.json()
+  # itemFAD = json.loads(httpFADResponse.text)
+  for lineValue in responseJson:
+    itemFAD = lineValue["first_aired_Date"]
+  # print(itemFAD)
 
-#------------------------------
-# Gather metadata from the report
-# for errorResults in root.iter('error'):
-#     errorMessage = errorResults.get('synopsis')
-#     errorDescription = errorResults.get('description')
-#     errorTimecode = errorResults.get('timecode')
-#     errorReport = errorReport + f"{errorTimecode} - {errorMessage} ({errorDescription})\n"
-#------------------------------
+  itemFAD = '2022-02-16T21:12:28' # this is only for testing
+  payloadCantemo = f'<MetadataDocument xmlns="http://xml.vidispine.com/schema/vidispine"><timespan start="-INF" end="+INF"><field><name>oly_firstUseDate</name><value>{itemFAD}</value></field></timespan></MetadataDocument>'
+  urlPutFirstUseDate = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_firstUseDate&terse=yes"
+  requests.request("PUT", urlPutFirstUseDate, headers=headersCantemo, data=payloadCantemo)
 
-#------------------------------
-# Update Cantemo metadata
-# headers = {
-#   'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
-#   'Cookie': 'csrftoken=HFOqrbk9cGt3qnc6WBIxWPjvCFX0udBdbJnzCv9jECumOjfyG7SS2lgVbFcaHBCc',
-#   'Content-Type': 'application/xml'
-# }
-# cantemoItemId = 'OLY-203'
-# urlPutAnalysisInfo = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata/"
-# payload = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_analysisReport</name><value>{errorReport}</value></field></timespan></MetadataDocument>"
-# httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=payload)
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
