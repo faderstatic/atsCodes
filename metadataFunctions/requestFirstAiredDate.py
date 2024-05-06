9cb15348d8d59004b27abade2d99c0e5783e778f
faderstatic <tang26.2@gmail.com>
2024-05-06 16:40:51 -0400

    Adding legacycontent logic

--- metadataFunctions/requestFirstAiredDate.py
+++ metadataFunctions/requestFirstAiredDate.py
@@ -22,49 +22,100 @@ from requests.exceptions import HTTPError
 try:
   cantemoItemId = sys.argv[1]
   # cantemoItemId = os.environ.get("portal_itemId")
+  ns = {'vidispine': 'http://xml.vidispine.com/schema/vidispine'}
 
   #------------------------------
-  # Making API call to Cantemo to get contract ID
+  # Making API call to Cantemo to get content flag
   headersCantemo = {
     'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
     'Cookie': 'csrftoken=HFOqrbk9cGt3qnc6WBIxWPjvCFX0udBdbJnzCv9jECumOjfyG7SS2lgVbFcaHBCc'
   }
   payloadCantemo = {}
-  urlGetContractId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineContractId&terse=yes"
-  httpApiResponse = requests.request("GET", urlGetContractId, headers=headersCantemo, data=payloadCantemo)
+  urlGetContentFlags = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_contentFlags&terse=yes&includeConstraintValue=all"
+  httpApiResponse = requests.request("GET", urlGetContentFlags, headers=headersCantemo, data=payloadCantemo)
   httpApiResponse.raise_for_status()
   #------------------------------
 
-  #------------------------------
-  # Parsing XML data to get Rightsline contract ID (rightslineContrackId)
-  ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
+    #------------------------------
+  # Parsing XML data to get content Flags (cantemoContentFlag)
+  # ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
   responseXml = httpApiResponse.text
-  root = ET.XML(responseXml)
-  ns = {'vidispine': 'http://xml.vidispine.com/schema/vidispine'}
+  flagRoot = ET.XML(responseXml)
   # print(root.tag, root.attrib, root.text)
-  for child in root.findall('vidispine:item', ns):
-    for cantemoContractId in child.findall('oly_rightslineContractId'):
-      rightslineContractId = cantemoContractId.text
+  for child in flagRoot.findall('vidispine:item', ns):
+    print(child.text)
+    for cantemoContentFlags in child.findall('oly_contentFlags'):
+      print(cantemoContentFlags.text)
   #------------------------------
+      if cantemoContentFlags.text == 'legacycontent':
+        #------------------------------
+        # Making API call to Cantemo to get contract code
+        payloadCantemo = {}
+        urlGetContractCode = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_contractCode&terse=yes"
+        httpApiResponse = requests.request("GET", urlGetContractCode, headers=headersCantemo, data=payloadCantemo)
+        httpApiResponse.raise_for_status()
+        #------------------------------
 
-   #------------------------------
-  # Making API call to Cantemo to get Rightsline Item ID
-  urlGetRightslineItemId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineItemId&terse=yes"
-  httpApiResponse = requests.request("GET", urlGetRightslineItemId, headers=headersCantemo, data=payloadCantemo)
-  httpApiResponse.raise_for_status()
-  #------------------------------
+        #------------------------------
+        # Parsing XML data to get Rightsline contract Code (contrackCode))
+        responseXml = httpApiResponse.text
+        root = ET.XML(responseXml)
+        for child in root.findall('vidispine:item', ns):
+          for cantemoContractId in child.findall('oly_contractCode'):
+            rightslineContractId = cantemoContractId.text
 
-  #------------------------------
-  # Parsing XML data to get Rightsline contract ID (rightslineContrackId)
-  ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
-  responseXml = httpApiResponse.text
-  root = ET.XML(responseXml)
-  ns = {'vidispine': 'http://xml.vidispine.com/schema/vidispine'}
-  # print(root.tag, root.attrib, root.text)
-  for child in root.findall('vidispine:item', ns):
-    for cantemoRightslineId in child.findall('oly_rightslineItemId'):
-      rightslineItemId = cantemoRightslineId.text
-  #------------------------------
+        #------------------------------
+        # Making API call to Cantemo to get Rightsline Item code
+        urlGetItemCode = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_itemCode&terse=yes"
+        httpApiResponse = requests.request("GET", urlGetItemCode, headers=headersCantemo, data=payloadCantemo)
+        httpApiResponse.raise_for_status()
+        #------------------------------
+
+        #------------------------------
+        # Parsing XML data to get Rightsline contract code (contractCode)
+        responseXml = httpApiResponse.text
+        root = ET.XML(responseXml)
+        # print(root.tag, root.attrib, root.text)
+        for child in root.findall('vidispine:item', ns):
+          for cantemoItemCode in child.findall('oly_itemCode'):
+            rightslineItemId = cantemoItemCode.text
+        #------------------------------
+        print(f'rightasLineContractId - rgihtslineItemId')
+      elif cantemoContentFlags:
+        #------------------------------
+        # Making API call to Cantemo to get contract ID
+        payloadCantemo = {}
+        urlGetContractId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineContractId&terse=yes"
+        httpApiResponse = requests.request("GET", urlGetContractId, headers=headersCantemo, data=payloadCantemo)
+        httpApiResponse.raise_for_status()
+        #------------------------------
+
+        #------------------------------
+        # Parsing XML data to get Rightsline contract ID (rightslineContractId)
+        responseXml = httpApiResponse.text
+        root = ET.XML(responseXml)
+        # print(root.tag, root.attrib, root.text)
+        for child in root.findall('vidispine:item', ns):
+          for cantemoContractId in child.findall('oly_rightslineContractId'):
+            rightslineContractId = cantemoContractId.text
+
+        #------------------------------
+        # Making API call to Cantemo to get Rightsline Item ID
+        urlGetRightslineItemId = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata?field=oly_rightslineItemId&terse=yes"
+        httpApiResponse = requests.request("GET", urlGetRightslineItemId, headers=headersCantemo, data=payloadCantemo)
+        httpApiResponse.raise_for_status()
+        #------------------------------
+
+        #------------------------------
+        # Parsing XML data to get Rightsline item ID (rightslineItemId)
+        # ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
+        responseXml = httpApiResponse.text
+        root = ET.XML(responseXml)
+        # print(root.tag, root.attrib, root.text)
+        for child in root.findall('vidispine:item', ns):
+          for cantemoRightslineId in child.findall('oly_rightslineItemId'):
+            rightslineItemId = cantemoRightslineId.text
+        #------------------------------
   
   #------------------------------
   # Making API call to Mira to get First Aired Date
