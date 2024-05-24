0adf5df39e4649ac9ccb52eaf79ea09704aa0837
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-05-24 13:26:28 -0400

    new script for reload process

--- olympusatWorkflows/reloadWorkflow/reloadWorkflow-prepareItemForReload_v1.0.sh
+++ olympusatWorkflows/reloadWorkflow/reloadWorkflow-prepareItemForReload_v1.0.sh
@@ -22,52 +22,133 @@ export datetime=$(date +%Y/%m/%d_%H:%M)
 
 logfile="/opt/olympusat/logs/olympusatWorkflow-$mydate.log"
 
-#Set Variable to check before continuing with script
+# Set Variable to check before continuing with script
 export itemId=$1
+export userName=$2
 itemVersionType=$(filterVidispineItemMetadata $itemId "metadata" "oly_versionType")
 
-#Check Variable
-if [[ "$itemVersionType" == "conformFile" || "$itemVersionType" == "censoredFile" ]];
+# Check versionType variable to make sure is conform, censored or original file
+if [[ "$itemVersionType" == "originalFile" || "$itemVersionType" == "conformFile" || "$itemVersionType" == "censoredFile" ]];
 then
-    #versionType IS 'conformFile' or 'censoredFile'-continue with process
+    # versionType IS 'originalFile', 'conformFile' or 'censoredFile'-continue with process
 
-    echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type IS 'conformFile' or 'censoredFile'" >> "$logfile"
-    echo "YEEESSSSS [$itemId] - Version Type IS 'conformFile' or 'censoredFile'"
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type IS 'originalFile', 'conformFile' or 'censoredFile'" >> "$logfile"
+
+    #---------------------------------------------------------------------------------------
+    # Get item QC metadata from Cantemo
+    itemOriginalQCStatus=$(filterVidispineItemMetadata $itemId "metadata" "oly_originalContentQCStatus")
+    itemFinalQCStatus=$(filterVidispineItemMetadata $itemId "metadata" "oly_finalQCStatus")
+
+    # Check item QC metadata & reset to Pending if necessary
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Checking & Updating QC Status Metadata" >> "$logfile"
+    sleep 1
+
+    if [[ "$itemOriginalQCStatus" == "approved" || "$itemOriginalQCStatus" == "rejected" ]];
+    then
+        echo "$datetime - (reloadWorkflow) - [$itemId] - Setting Original QC Status to 'Pending'" >> "$logfile"
+
+        updateVidispineMetadata $itemId "oly_originalContentQCStatus" "pending"
+
+        sleep 1
+    fi
+
+    if [[ "$itemFinalQCStatus" == "approved" || "$itemFinalQCStatus" == "rejected" ]];
+    then
+        echo "$datetime - (reloadWorkflow) - [$itemId] - Setting Final QC Status to 'Pending'" >> "$logfile"
+
+        updateVidispineMetadata $itemId "oly_finalQCStatus" "pending"
+
+        sleep 1
+    fi
+    #---------------------------------------------------------------------------------------
+
+    #---------------------------------------------------------------------------------------
+    # Get item Archive metadata from Cantemo
+    itemQuantumArchiveStatus=$(filterVidispineItemMetadata $itemId "metadata" "portal_archive_status")
+    itemAWSArchiveStatus=$(filterVidispineItemMetadata $itemId "metadata" "oly_archiveStatusAWS")
+    itemAWSArchiveId=$(filterVidispineItemMetadata $itemId "metadata" "oly_archiveIdAWS")
+
+    # Check item Archive metadata & delete from Archive if necessary
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Checking if previously Archived" >> "$logfile"
+    sleep 1
+
+    if [[ "$itemAWSArchiveStatus" == "completed" ]];
+    then
+        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting previous file from AWS Glacier" >> "$logfile"
+
+        #bash -c "sudo /opt/olympusat/scriptsActive/glacierDeleteV1.sh $itemId $userName > /dev/null 2>&1 &"
+
+        sleep 1
+    fi
+
+    if [[ "$itemQuantumArchiveStatus" == *Archived* ]];
+    then
+        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting previous file from Quantum Archive" >> "$logfile"
+
+        # Insert code/command to trigger Delete from Quantum Archive here
+
+        sleep 1
+    fi
+    #---------------------------------------------------------------------------------------
+
+    #---------------------------------------------------------------------------------------
+    # Get item's original shape information from Cantemo
+    urlGetItemInfo="http://10.1.1.34:8080/API/item/$itemId/shape?tag=original"
+	httpResponse=$(curl --location --request GET $urlGetItemInfo --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=Tkb9vkSC8v4SceB8CHUyB3iaMPjvgoHrzhLrvo36agG3wqv0jHc7nsOtdTo9JEyM')
+	originalShapeId=$(echo $httpResponse | awk -F "<uri>" '{print $2}' | awk -F "</uri>" '{print $1}')
+	
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Original Shape ID {$originalShapeId}" >> "$logfile"
+
+    # Get item's lowres shape information from Cantemo
+    urlGetItemInfo="http://10.1.1.34:8080/API/item/$itemId/shape?tag=lowres"
+	httpResponse=$(curl --location --request GET $urlGetItemInfo --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=Tkb9vkSC8v4SceB8CHUyB3iaMPjvgoHrzhLrvo36agG3wqv0jHc7nsOtdTo9JEyM')
+	lowresShapeId=$(echo $httpResponse | awk -F "<uri>" '{print $2}' | awk -F "</uri>" '{print $1}')
+	
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Lowres Shape ID {$originalShapeId}" >> "$logfile"
+
+    sleep 1
+
+    # Check original shape id exists & delete from Cantemo
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Checking if Original Shape ID exists" >> "$logfile"
+    sleep 1
+
+    if [[ ! -z "$originalShapeId" ]];
+    then
+        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting Original Shape from Cantemo & Filesystem" >> "$logfile"
+
+        # Insert code/command to trigger Delete of Shape in Cantemo here
+
+        sleep 1
+    fi
+
+    # Check lowres shape id exists & delete from Cantemo
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Checking if Lowres Shape ID exists" >> "$logfile"
+    sleep 1
+
+    if [[ ! -z "$lowresShapeId" ]];
+    then
+        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting Lowres Shape from Cantemo & Filesystem" >> "$logfile"
+
+        # Insert code/command to trigger Delete of Shape in Cantemo here
+
+        sleep 1
+    fi
+    #---------------------------------------------------------------------------------------
+
+    sleep 5
+
+    #---------------------------------------------------------------------------------------
+    # Reset Item to Placeholder
+
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Resetting Item to Placeholder" >> "$logfile"
+
+    # Insert code/command to trigger Reset to Placeholder on Item in Cantemo here
+
+    #---------------------------------------------------------------------------------------
 
 else
-    if [[ "$itemVersionType" == "originalFile" ]];
-    then
-        #versionType IS 'originalFile'-contine with process
-
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type IS 'originalFile'" >> "$logfile"
-        echo "YEEESSSSS [$itemId] - Version Type IS 'originalFile'"
-
-    else
-        #versionType IS NOT 'conformFile' nor 'censoredFile' nor 'originalFile'-skip process
-
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type is NOT 'conformFile' nor 'censoredFile' nor 'originalFile'" >> "$logfile"
-        echo "NOOOOOOOO [$itemId] - Version Type is NOT 'conformFile' nor 'censoredFile' nor 'originalFile'"
-
-    fi
+    # versionType IS NOT 'originalFile, 'conformFile' nor 'censoredFile'-skip process
+    echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type is NOT 'originalFile', 'conformFile' nor 'censoredFile'" >> "$logfile"
 fi
-
-case "$itemVersionType" in
-
-    "conformFile"|"censoredFile")
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Case - Version Type IS 'conformFile' or 'censoredFile'" >> "$logfile"
-        echo "Case-YEEESSSSS [$itemId] - Version Type IS 'conformFile' or 'censoredFile'"
-    ;;
-
-    "originalFile")
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Case - Version Type IS 'originalFile'" >> "$logfile"
-        echo "Case-YEEESSSSS [$itemId] - Version Type IS 'originalFile'"
-    ;;
-
-    *)
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Case - Version Type is NOT 'conformFile' nor 'censoredFile' nor 'originalFile'" >> "$logfile"
-        echo "Case-NOOOOOOOO [$itemId] - Version Type is NOT 'conformFile' nor 'censoredFile' nor 'originalFile'"
-    ;;
-
-esac
 
 IFS=$saveIFS
