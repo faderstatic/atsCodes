bd2d43e4ee24df89ac52186e25ef112674eadc61
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-06-17 08:04:01 -0400

    tweaks/updates

--- olympusatWorkflows/reloadWorkflow/reloadWorkflow-prepareItemForReload_v1.2.sh
+++ olympusatWorkflows/reloadWorkflow/reloadWorkflow-prepareItemForReload_v1.2.sh
@@ -18,7 +18,7 @@ saveIFS=$IFS
 IFS=$(echo -e "\n\b")
 
 export mydate=$(date +%Y-%m-%d)
-export datetime=$(date +%Y/%m/%d_%H:%M)
+export datetime=$(date +%Y/%m/%d_%H:%M:%S)
 
 logfile="/opt/olympusat/logs/olympusatWorkflow-$mydate.log"
 
@@ -32,7 +32,7 @@ if [[ "$itemVersionType" == "originalFile" || "$itemVersionType" == "conformFile
 then
     # versionType IS 'originalFile', 'conformFile' or 'censoredFile'-continue with process
 
-    echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type IS 'originalFile', 'conformFile' or 'censoredFile'" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Version Type IS 'originalFile', 'conformFile' or 'censoredFile'" >> "$logfile"
 
     #---------------------------------------------------------------------------------------
     # Get item QC metadata from Cantemo
@@ -40,23 +40,30 @@ then
     itemFinalQCStatus=$(filterVidispineItemMetadata $itemId "metadata" "oly_finalQCStatus")
 
     # Check item QC metadata & reset to Pending if necessary
-    echo "$datetime - (reloadWorkflow) - [$itemId] - Checking & Updating QC Status Metadata" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Checking & Updating QC Status Metadata" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Original Content QC Status - {$itemOriginalQCStatus}" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Final QC Status - {$itemFinalQCStatus}" >> "$logfile"
+
     sleep 1
 
-    if [[ "$itemOriginalQCStatus" == "approved" || "$itemOriginalQCStatus" == "rejected" ]];
+    if [[ "$itemOriginalQCStatus" == "pending" || "$itemOriginalQCStatus" == "approved" || "$itemOriginalQCStatus" == "rejected" ]];
     then
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Setting Original QC Status to 'Pending'" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Setting Original QC Status to 'Pending'" >> "$logfile"
 
         updateVidispineMetadata $itemId "oly_originalContentQCStatus" "pending"
+        updateVidispineMetadata $itemId "oly_originalContentQCBy" ""
+        updateVidispineMetadata $itemId "oly_originalContentQCDate" ""
 
         sleep 1
     fi
 
     if [[ "$itemFinalQCStatus" == "approved" || "$itemFinalQCStatus" == "rejected" ]];
     then
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Setting Final QC Status to 'Pending'" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Setting Final QC Status to 'Pending'" >> "$logfile"
 
         updateVidispineMetadata $itemId "oly_finalQCStatus" "pending"
+        updateVidispineMetadata $itemId "oly_finalQCBy" ""
+        updateVidispineMetadata $itemId "oly_finalQCDate" ""
 
         sleep 1
     fi
@@ -69,32 +76,37 @@ then
     itemAWSArchiveId=$(filterVidispineItemMetadata $itemId "metadata" "oly_archiveIdAWS")
 
     # Check item Archive metadata & delete from Archive if necessary
-    echo "$datetime - (reloadWorkflow) - [$itemId] - Checking if previously Archived" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Checking if previously Archived" >> "$logfile"
     sleep 1
 
     if [[ "$itemAWSArchiveStatus" == "completed" ]];
     then
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting previous file from AWS Glacier" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Deleting previous file from AWS Glacier" >> "$logfile"
 
-        #bash -c "sudo /opt/olympusat/scriptsActive/glacierDeleteV1.sh $itemId $userName > /dev/null 2>&1 &"
-        #updateVidispineMetadata $itemId oly_archiveStatusAWS ""
-        #updateVidispineMetadata $itemId oly_archiveDateAWS ""
-        #updateVidispineMetadata $itemId oly_archiveIdAWS ""
-        #updateVidispineMetadata $itemId oly_flushDate ""
-        #updateVidispineMetadata $itemId oly_flushStatus ""
-        #updateVidispineMetadata $itemId oly_restoreStatusAWS ""
-        #updateVidispineMetadata $itemId oly_restoreDateAWS ""
+        bash -c "sudo /opt/olympusat/scriptsActive/glacierDeleteV1.sh $itemId $userName > /dev/null 2>&1 &"
+        updateVidispineMetadata $itemId oly_archiveStatusAWS ""
+        updateVidispineMetadata $itemId oly_archiveDateAWS ""
+        updateVidispineMetadata $itemId oly_archiveIdAWS ""
+        updateVidispineMetadata $itemId oly_flushDate ""
+        updateVidispineMetadata $itemId oly_flushStatus ""
+        updateVidispineMetadata $itemId oly_restoreStatusAWS ""
+        updateVidispineMetadata $itemId oly_restoreDateAWS ""
 
         sleep 1
     fi
 
     if [[ "$itemQuantumArchiveStatus" == "Archived/Restored" ]];
     then
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting previous file from Quantum Archive" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Deleting previous file from Quantum Archive" >> "$logfile"
 
-        # Insert code/command to trigger Delete from Quantum Archive here
+        urlDeleteFromQuantumArchive="http://10.1.1.34/archive_framework/items/delete/"
+        deleteFromQuantumBody="{ \"ignore_list\": [], \"item_ids\": [\"$itemId\"]}"
+	    deleteFromQuantumHttpResponse=$(curl --location --request POST $urlDeleteFromQuantumArchive --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CRbBvVEFSfR5lHoQebsbQemRRas2MUyo53CsO5ixtkSrzvC9H7NffcuaXkIJvr1V' --data $deleteFromQuantumBody)
 
-        sleep 1
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Response from Cantemo {$deleteFromQuantumHttpResponse}" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Delay 10 sec - Check for Shape Information" >> "$logfile"
+
+        sleep 10
     fi
     #---------------------------------------------------------------------------------------
 
@@ -105,14 +117,16 @@ then
 
     if [[ "$httpResponse" == *'<uri>OLY-'* ]];
     then
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Deleting Shapes from Cantemo & Filesystem" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Deleting Shapes from Vidispine, Cantemo & Filesystem" >> "$logfile"
 
         urlDeleteShape="http://10.1.1.34:8080/API/item/$itemId/shape"
-	    #httpResponse=$(curl --location --request DELETE $urlDeleteShape --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=Tkb9vkSC8v4SceB8CHUyB3iaMPjvgoHrzhLrvo36agG3wqv0jHc7nsOtdTo9JEyM')
+	    deleteShapeHttpResponse=$(curl --location --request DELETE $urlDeleteShape --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=Tkb9vkSC8v4SceB8CHUyB3iaMPjvgoHrzhLrvo36agG3wqv0jHc7nsOtdTo9JEyM')
+
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Response from Vidispine {$deleteShapeHttpResponse}" >> "$logfile"
 
         sleep 1
     else
-        echo "$datetime - (reloadWorkflow) - [$itemId] - No Shape(s) for Item in Cantemo" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - No Shape(s) for Item in Cantemo" >> "$logfile"
     fi
     #---------------------------------------------------------------------------------------
 
@@ -120,38 +134,41 @@ then
 
     #---------------------------------------------------------------------------------------
     # Check to confirm shapes were deleted
+    
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Checking to make sure Shapes were deleted" >> "$logfile"
+
     urlGetItemShapeInfo="http://10.1.1.34:8080/API/item/$itemId/shape"
 	httpResponse=$(curl --location --request GET $urlGetItemShapeInfo --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=Tkb9vkSC8v4SceB8CHUyB3iaMPjvgoHrzhLrvo36agG3wqv0jHc7nsOtdTo9JEyM')
 
     if [[ "$httpResponse" != *'<uri>OLY-'* ]];
     then
-        echo "$datetime - (reloadWorkflow) - [$itemId] - No Shapes Exist - Resetting Item to Placeholder" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - No Shapes Exist - Resetting Item to Placeholder" >> "$logfile"
 
         # Reset Item to Placeholder
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Resetting Item to Placeholder" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Resetting Item to Placeholder" >> "$logfile"
         itemTitle=$(filterVidispineItemMetadata $itemId "metadata" "title")
 
         urlResetToPlaceholder="http://10.1.1.34/API/v2/items/$itemId/formats/"
-	    #httpResponse=$(curl --location --request DELETE $urlResetToPlaceholder --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CUhJ240QCYOVA1GW8yARnGbTGKlcVxuMWuEabYLspdpzlmjwcaC1J91bABx7f2RW')
+	    httpResponse=$(curl --location --request DELETE $urlResetToPlaceholder --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=CUhJ240QCYOVA1GW8yARnGbTGKlcVxuMWuEabYLspdpzlmjwcaC1J91bABx7f2RW')
 
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Item Reset to Placeholder Completed" >> "$logfile"
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Updating Item Title {$itemTitle}" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Item Reset to Placeholder Completed" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Updating Item Title {$itemTitle}" >> "$logfile"
 
         sleep 5
 
         #updateVidispineMetadata $itemId "title" "$itemTitle"
 
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Prepare for Reload Workflow Completed" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Prepare for Reload Workflow Completed" >> "$logfile"
 
     else
-        echo "$datetime - (reloadWorkflow) - [$itemId] - Shape(s) still exist for Item in Cantemo - Item NOT Reset to Placeholder" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Shape(s) still exist for Item in Cantemo - Item NOT Reset to Placeholder" >> "$logfile"
     fi
 
     #---------------------------------------------------------------------------------------
 
 else
     # versionType IS NOT 'originalFile, 'conformFile' nor 'censoredFile'-skip process
-    echo "$datetime - (reloadWorkflow) - [$itemId] - Version Type is NOT 'originalFile', 'conformFile' nor 'censoredFile'" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reloadWorkflow) - [$itemId] - Version Type is NOT 'originalFile', 'conformFile' nor 'censoredFile'" >> "$logfile"
 fi
 
 IFS=$saveIFS
