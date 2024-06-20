eb2b60f7fc216469017a885107c30b57085633b9
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-06-20 13:07:12 -0400

    [81] to [84] adding lines for troubleshooting

--- metadataFunctions/aiFingerprintIngest.py
+++ metadataFunctions/aiFingerprintIngest.py
@@ -78,6 +78,10 @@ try:
   httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=moodPayload)
   time.sleep(5)
   httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=keywordPayload)
+  keywordApiFile = f"/mnt/c/Users/kkanjanapitak/Desktop/{cantemoItemId}_FP.json"
+  keywordWriting = open(keywordApiFile, "w")
+  keywordWriting.write(keywordPayload)
+  keywordWriting.close()
   #------------------------------
   #------------------------------
 
