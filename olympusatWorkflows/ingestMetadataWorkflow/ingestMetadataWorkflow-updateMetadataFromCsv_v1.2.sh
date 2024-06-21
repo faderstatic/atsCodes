c9af3b2bf9ef9fe876c6a4068f3d9ad6b99c9374
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-06-21 11:23:08 -0400

    uncommenting out the http post to import xml and command to move xml after importing

--- olympusatWorkflows/ingestMetadataWorkflow/ingestMetadataWorkflow-updateMetadataFromCsv_v1.2.sh
+++ olympusatWorkflows/ingestMetadataWorkflow/ingestMetadataWorkflow-updateMetadataFromCsv_v1.2.sh
@@ -488,7 +488,7 @@ then
             echo "$(date +%Y/%m/%d_%H:%M:%S) - (initialIngestMetadata) - [$cantemoItemId] - Triggering API Call to Import XML into Cantemo" >> "$logfile"
 
             url="http://10.1.1.34:8080/API/import/sidecar/$cantemoItemId?sidecar=/opt/olympusat/xmlsForMetadataImport/$cantemoItemId.xml"
-            #importXmlHttpResponse=$(curl --location --request POST $url --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0')
+            importXmlHttpResponse=$(curl --location --request POST $url --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0')
 
             sleep 2
             
@@ -496,7 +496,7 @@ then
 
             sleep 2
 
-            #mv "$fileDestination" "/opt/olympusat/xmlsForMetadataImport/zCompleted/"
+            mv "$fileDestination" "/opt/olympusat/xmlsForMetadataImport/zCompleted/"
 
             echo "$(date +%Y/%m/%d_%H:%M:%S) - (initialIngestMetadata) - [$cantemoItemId] - Import Metadata Job Completed" >> "$logfile"
 
