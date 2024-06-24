530557826232a0e3485618ad40d5054dca096e75
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-06-24 09:01:45 -0400

    fix json parsing

--- metadataFunctions/aiFingerprintIngestV1.py
+++ metadataFunctions/aiFingerprintIngestV1.py
@@ -43,11 +43,9 @@ try:
   responseJson = httpApiResponse.json() if httpApiResponse and httpApiResponse.status_code == 200 else None
   if responseJson and 'field' in responseJson:
     for fieldInformation in responseJson['field']:
-      for keywordType in fieldInformation['key']:
-        print(keywordType)
-        if keywordType == "__values":
-          keywordValueXML = fieldInformation['value']
-          print(keywordValueXML)
+      if fieldInformation['key'] == "__values":
+        keywordValueXML = fieldInformation['value']
+        print(keywordValueXML)
   
   #------------------------------
   # Making API call to Vionlabs to get fingerprints
