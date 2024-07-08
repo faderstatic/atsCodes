c2c3a4a26ae99ca820e9d8c69f540a376a4dd750
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-07-08 08:36:22 -0400

    changes/tweaks

--- olympusatWorkflows/notificationWorkflow/notificationWorkflow-newItemIngested_v1.0.sh
+++ olympusatWorkflows/notificationWorkflow/notificationWorkflow-newItemIngested_v1.0.sh
@@ -23,47 +23,47 @@ logfile="/opt/olympusat/logs/olympusatWorkflow-$mydate.log"
 
 # Set Variables to check before continuing with script
 export itemId=$1
-export emailWorkflow=$2
+export emailNotificationWorkflow=$2
 
-echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - Email Workflow - [$emailWorkflow]" >> "$logfile"
+echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - Email Workflow - [$emailNotificationWorkflow]" >> "$logfile"
 
 # Check Variable
-if [[ "$emailWorkflow" == *"newItem"* ]];
+if [[ "$emailNotificationWorkflow" == *"newItem"* ]];
 then
-    # emailWorkflow varialbe is set to newItem
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - Checking for newItemFileDestination file" >> "$logfile"
+    # emailNotificationWorkflow varialbe is set to newItem
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - Checking for newItemFileDestination file" >> "$logfile"
     newItemFileDestination="/opt/olympusat/resources/emailNotificationWorkflow/newItem/newItemWorkflow-$mydate.csv"
     if [[ ! -e "$newItemFileDestination" ]];
     then
-        echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - newItemFileDestination file NOT FOUND - creating new file with headers" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - newItemFileDestination file NOT FOUND - creating new file with headers" >> "$logfile"
 
         sleep 2
 
-        echo "ItemId,ContentType,VersionType" >> "$newItemFileDestination"
+        echo "ItemId,Title,ContentType,VersionType" >> "$newItemFileDestination"
 
-        echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - New File created - [$newItemFileDestination]" >> "$logfile"
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - New File created - [$newItemFileDestination]" >> "$logfile"
         
         sleep 5
     fi 
 
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - Gathering item metadata from Cantemo" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - Gathering item metadata from Cantemo" >> "$logfile"
     itemTitle=$(filterVidispineItemMetadata $itemId "metadata" "title")
     itemContentType=$(filterVidispineItemMetadata $itemId "metadata" "oly_contentType")
     itemVersionType=$(filterVidispineItemMetadata $itemId "metadata" "oly_versionType")
 
     sleep 2
 
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - Adding item metadata to newItemWorkflow csv" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - Adding item metadata to newItemWorkflow csv" >> "$logfile"
 
-    echo "$itemId,$itemContentType,$itemVersionType" >> "$newItemFileDestination"
+    echo "$itemId,$itemTitle,$itemContentType,$itemVersionType" >> "$newItemFileDestination"
 
     sleep 2
 
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - Process completed" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - Process completed" >> "$logfile"
 
 else
-    # emailWorkflow variable is not supported
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailWorkflow) - ($itemId) - emailWorkflow variable is not supported" >> "$logfile"
+    # emailNotificationWorkflow variable is not supported
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (emailNotificationWorkflow) - ($itemId) - emailNotificationWorkflow variable is not supported" >> "$logfile"
 fi
 
 IFS=$saveIFS
