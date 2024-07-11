dbd7b35b53d04f0625bcd48bb9af1be1d26f58fd
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-07-11 11:18:09 -0400

    [64]

--- metadataFunctions/aiFingerprintIngestV1.py
+++ metadataFunctions/aiFingerprintIngestV1.py
@@ -61,7 +61,7 @@ def readCantimoLookup(rclFieldName):
   return lookupList
 
 def createCantimoLookup(cckFieldName, cckLookupXMLPayload):
-  print(f"{cckLookupXMLPayload} will be PUT")
+  print(f"{cckFieldName} is updated with payload... {cckLookupXMLPayload})
   pass
 
 #------------------------------
