868e7bf0122a18cf02503bc8ec6519c620d55089
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-07-22 15:15:04 -0400

    tweaked email to list

--- olympusatWorkflows/notificationWorkflow/dailyReport/notificationWorkflow-sendEmailWithReport_v2.1.sh
+++ olympusatWorkflows/notificationWorkflow/dailyReport/notificationWorkflow-sendEmailWithReport_v2.1.sh
@@ -338,7 +338,7 @@ MAM Notify"
                     curl --url 'smtp://smtp-mail.outlook.com:587' \
                     --ssl-reqd  \
                     --mail-from $emailFrom \
-                    --mail-rcpt $recipient1 --mail-rcpt $recipient2 --mail-rcpt $recipient3 \
+                    --mail-rcpt $recipient1 \
                     --user 'notify@olympusat.com:6bOblVsLg9bPQ8WG7JC7f8Zump' \
                     -F '=(;type=multipart/mixed' \
                     -F "=$sesMessage;type=text/plain" \
