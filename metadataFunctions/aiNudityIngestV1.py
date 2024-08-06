b1fdd2e3feb5e0676e5722028c2d85043c2675c8
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-08-06 09:15:13 -0400

    remove comments

--- metadataFunctions/aiNudityIngestV1.py
+++ metadataFunctions/aiNudityIngestV1.py
@@ -128,7 +128,7 @@ try:
       urlPutProfanityInfo = f"http://10.1.1.34/AVAPI/asset/{cantemoItemId}/timespan/bulk"
       httpApiResponse = requests.request("PUT", urlPutProfanityInfo, headers=headers, data=segmentPayload)
       httpApiResponse.raise_for_status()
-      print(httpApiResponse.text)
+      # print(httpApiResponse.text)
       time.sleep(3)
       #------------------------------
 
