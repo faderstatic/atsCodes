d683f0e4470460d8cf544a624a31b75c3aaf5e64
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-08-30 07:49:05 -0400

    for Tang

---
+++ zTangWorkingFolder/awsElementalPresetExport.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#::***************************************************************************************************************************
+#::This shell script will get metadata from Original Master Episode & copy to other Master Types (textless, dubbed, etc)
+#::Engineers: Ryan Sims & Tang Kanjanapitak
+#::Client: Olympusat
+#::Updated: 05/14/2024
+#::Rev A: 
+#::System requirements: This script will run in LINUX & MacOS
+#::***************************************************************************************************************************
+
+saveIFS=$IFS
+IFS=$(echo -e "\n\b")
+
+for i in {1..10}
+    do
+        curl -s -o preset_${i}.xml -f http://172.16.1.120/api/presets/${i}.xml?clean=true
+    done
+
+IFS=$saveIFS
