7311d2eaa9cc98bfff2480714d72d54961147c8d
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-09-03 07:02:42 -0400

    update md5 sum process to accept 32 digits

--- zTangWorkingFolder/awsElementalPresetExport.sh
+++ zTangWorkingFolder/awsElementalPresetExport.sh
@@ -12,13 +12,39 @@
 saveIFS=$IFS
 IFS=$(echo -e "\n\b")
 
+<<<<<<< HEAD
+eventUrl=$1
+maxPresets=$2
+destinationFolder=$3
+
+export apiKey="FbABGpvsgGDkTKUZchLv"
+# export authExpire=$(date -d "+5 minutes" +"%s")
+export authUser="admin"
+=======
 export apiUserKey="FbABGpvsgGDkTKUZchLv"
 export authExpire=$(date -v +7d +"%s")
 export md5ForApiUserKey=$(echo -n "$apiUserKey" | md5)
+>>>>>>> a97eefad0924a2c5a1f08e7c872cc18e988510ed
+
+apiKeyMd5=$(echo $apiKey | md5sum | awk '{print $1}')
+apiKeyMd5Dec=$((16#$apiKeyMd5))
 
 for i in {1..10}
-    do
-        curl -s -o preset_${i}.xml -f http://172.16.1.120/api/presets/${i}.xml?clean=true
-    done
+do
+    authExpire=$(date -d "+2 seconds" +"%s")
+    apiParameter="/"$eventUrl"/"$i$authUser$apiKey$authExpire
+    apiParameterMd5=$(echo $apiParameter | md5sum | awk '{print $1}')
+    apiParameterMd5CAP=$(echo "$apiParameterMd5" | tr '[:lower:]' '[:upper:]')
+    apiParameterMd5=$$(($apiParameterMd5CAP))
+    apiParameterMd5Dec=$(echo "ibase=16;apiParameter)
+    finalAuthenticationDec=$(($apiKeyMd5Dec+$apiParameterMd5Dec))
+    printf -v apiParameterMd5Comp "%x" $apiParameterMd5Dec
+    echo $apiParameterMd5Comp" compares to original "$apiParameterMd5
+    # printf -v finalAuthentication "%x" $finalAuthenticationDec
+    # printf -v finalAuthentication "%x\n" $((16#$apiParameterMd5 + 16#$apiKeyMd5))
+    finalAuthentication=$(echo "obase=16;ibase=16;$apiParameter+$apiKey" | bc)
+    echo $finalAuthentication
+    # curl -s -o preset_$i.xml -f http://172.16.1.120/api/$eventUrl/$i.xml?clean=true
+done
 
 IFS=$saveIFS
