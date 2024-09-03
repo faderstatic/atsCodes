375335f5bc2bb93d857245b0d8efd95382bca943
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-09-03 13:40:14 -0400

    first test get aws presets

--- zTangWorkingFolder/awsElementalPresetExport.py
+++ zTangWorkingFolder/awsElementalPresetExport.py
@@ -1,5 +1,5 @@
-#!/opt/cantemo/python/bin/python
-# /usr/bin/python3
+# /opt/cantemo/python/bin/python
+#!/usr/local/bin/python3
 
 # This application ingests metadata from Baton XML output file into Cantemo
 # PREREQUISITE: -none-
@@ -18,11 +18,46 @@ import xml.etree.ElementTree as ET
 import hashlib
 #------------------------------
 
-eventUrl = sys.argv[1]
-maxPresets = sys.argv[2]
-destinationFolder = sys.argv[3]
+authUser = sys.argv[1]
+eventUrl = sys.argv[2]
+maxPresets = int(sys.argv[3]) + 1
+destinationFolder = sys.argv[4]
+timeOffset = 30
 apiUserKey = "FbABGpvsgGDkTKUZchLv"
-md5ForApiUserKey = hashlib.md5(apiUserKey).hexdigest()
+apiUserKeyEncoded = apiUserKey.encode('utf-8')
+apiUserKeyMd5 = hashlib.md5(apiUserKeyEncoded).hexdigest()
 
 try:
-    print(md5ForApiUserKey)
+    for i in range(1,maxPresets):
+        executionTime = datetime.datetime.now()
+        # executionTimeUnix = int(round(executionTime.timestamp()))
+        authExpiration = executionTime + datetime.timedelta(seconds=timeOffset)
+        authExpirationUnix = int(round(authExpiration.timestamp()))
+        # print(f"{executionTimeUnix} - {authExpirationUnix}")
+        authExpirationStr = str(authExpirationUnix)
+        authExpirationEncoded = authExpirationStr.encode("utf-8")
+        authExpirationMd5 = hashlib.md5(authExpirationEncoded).hexdigest()
+        # print(f"{apiUserKeyMd5} - {authExpirationMd5}")
+        apiParameter="/" + eventUrl + "/" + str(i) + authUser + apiUserKey + authExpirationStr
+        apiParameterEncoded = apiParameter.encode("utf-8")
+        apiParameterMd5 = hashlib.md5(apiParameterEncoded).hexdigest()
+        fullParameterMd5 = hex(int(apiUserKeyMd5, 16) + int(apiParameterMd5, 16))[2:]
+        # print(fullParameterMd5)
+        #------------------------------
+        # Making API to AWS directory to xml files
+        headers = {
+            'X-Auth-User': 'admin',
+            'X-Auth-Expires': f'{authExpirationUnix}',
+            'X-AuthKey': f'{fullParameterMd5}',
+            'Accept': 'application/xml'
+        }
+        urlGetElement = f"http://172.16.1.120/{eventUrl}/{i}.xml?clean=true"
+        payload = {}
+        httpApiResponse = requests.request("GET", urlGetElement, headers=headers, data=payload)
+        httpApiResponse.raise_for_status()
+        #------------------------------
+
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
