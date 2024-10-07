f58d8e1bb50c335667e904efa7725d1b2eec5fc3
rmsims77 <167781877+rmsims77@users.noreply.github.com>
2024-10-07 09:54:33 -0400

    updates & tweaks

--- olympusatWorkflows/adComplianceWorkflow/acWorkflow-updateReviewStatus_v1.1.sh
+++ olympusatWorkflows/adComplianceWorkflow/acWorkflow-updateReviewStatus_v1.1.sh
@@ -49,84 +49,94 @@ touch "$lockFile"
 # Ensure that the lock is released when the job finishes
 trap releaseLock EXIT
 # --------------------------------------------------
-export url="http://10.1.1.34:8080/API/item/$itemId/metadata/"
-export reviewBy=$2
-export reviewDate=$(date "+%Y-%m-%dT%H:%M:%S")
-itemReviewStatus=$(filterVidispineItemMetadata $itemId "metadata" "ac_reviewStatus")
-export newItem=0
-echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Current Review Status - {$itemReviewStatus}" >> "$logfile"
-if [[ "$reviewStatus" == "newItem-pending" ]];
+# API call to get metadata_main_group
+getMetadataMainGroupUrl="http://10.1.1.34:8080/API/item/$itemId/metadata?group=null"
+getMetadataMainGroupHttpResponse=$(curl --location $getMetadataMainGroupUrl --header 'Accept: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=KPo8XvLM97mfZdNhFOicHLu2WgZsZWT9z0xMBG5cTV1jbmao22hTEekmK845PRhq')
+itemMetadataMainGroup=$(echo "$getMetadataMainGroupHttpResponse" | awk -F '</group>' '{print $1}' | awk -F '<group>' '{print $2}')
+#echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Main Metadata Group {$itemMetadataMainGroup}" >> "$logfile"
+if [[ "$itemMetadataMainGroup" == "Ad Compliance" ]];
 then
-    export reviewStatus="pending"
-    export newItem=1
-    if [[ "$reviewStatus" == "pending" ]];
+    export url="http://10.1.1.34:8080/API/item/$itemId/metadata/"
+    export reviewBy=$2
+    export reviewDate=$(date "+%Y-%m-%dT%H:%M:%S")
+    itemReviewStatus=$(filterVidispineItemMetadata $itemId "metadata" "ac_reviewStatus")
+    export newItem=0
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Metadata Main Group - [$itemMetadataMainGroup] - Current Review Status - {$itemReviewStatus}" >> "$logfile"
+    if [[ "$reviewStatus" == "newItem-pending" ]];
+    then
+        export reviewStatus="pending"
+        export newItem=1
+        if [[ "$reviewStatus" == "pending" ]];
+        then
+            bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field></timespan></MetadataDocument>")
+            if [[ "$newItem" -eq 1 ]];
+            then
+                echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Triggering Add to Daily Report on New Item" >> "$logfile"
+                bash -c "sudo /opt/olympusat/scriptsActive/notificationWorkflow-addToDailyReport_v2.5.sh $itemId adComplianceNewItem > /dev/null 2>&1 &"
+            fi
+        else
+            echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - New Review Status NOT Supported {$reviewStatus}" >> "$logfile"
+        fi
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
+    #elif [[ "$reviewStatus" == "pending" && "$itemReviewStatus" == "" ]];
+    elif [[ "$reviewStatus" == "pending" ]];
     then
         bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field></timespan></MetadataDocument>")
-        if [[ "$newItem" -eq 1 ]];
-        then
-            echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Triggering Add to Daily Report on New Item" >> "$logfile"
-            bash -c "sudo /opt/olympusat/scriptsActive/notificationWorkflow-addToDailyReport_v2.5.sh $itemId adComplianceNewItem > /dev/null 2>&1 &"
-        fi
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
+    #elif [[ "$reviewStatus" == "inProgress" && "$itemReviewStatus" == "pending" ]];
+    elif [[ "$reviewStatus" == "inProgress" ]];
+    then
+        bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field></timespan></MetadataDocument>")
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
+    elif [[ "$reviewStatus" == "needsSupportingDocuments" ]];
+    then
+        bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
+    elif [[ "$reviewStatus" == "approved" || "$reviewStatus" == "approvedWithRestrictions" || "$reviewStatus" == "noApparentConcerns" ]];
+    then
+        bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
+    elif [[ "$reviewStatus" == "rejected" ]];
+    then
+        bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
+    elif [[ "$reviewStatus" == "other" ]];
+    then
+        bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
+        #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
+        sleep 1
+        httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
+        sleep 2
+        echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
     else
         echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - New Review Status NOT Supported {$reviewStatus}" >> "$logfile"
     fi
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
-#elif [[ "$reviewStatus" == "pending" && "$itemReviewStatus" == "" ]];
-elif [[ "$reviewStatus" == "pending" ]];
-then
-    bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field></timespan></MetadataDocument>")
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
-#elif [[ "$reviewStatus" == "inProgress" && "$itemReviewStatus" == "pending" ]];
-elif [[ "$reviewStatus" == "inProgress" ]];
-then
-    bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field></timespan></MetadataDocument>")
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
-elif [[ "$reviewStatus" == "needsSupportingDocuments" ]];
-then
-    bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
-elif [[ "$reviewStatus" == "approved" || "$reviewStatus" == "approvedWithRestrictions" || "$reviewStatus" == "noApparentConcerns" ]];
-then
-    bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
-elif [[ "$reviewStatus" == "rejected" ]];
-then
-    bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
-elif [[ "$reviewStatus" == "other" ]];
-then
-    bodyData=$(echo "<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>ac_reviewStatus</name><value>$reviewStatus</value></field><field><name>ac_reviewBy</name><value>$reviewBy</value></field><field><name>ac_reviewDate</name><value>$reviewDate</value></field></timespan></MetadataDocument>")
-    #echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Body Data - [$bodyData]" >> "$logfile"
-    sleep 1
-    httpResponse=$(curl -s -o /dev/null --location --request PUT $url --header 'Content-Type: application/xml' --header 'Authorization: Basic YWRtaW46MTBsbXBAc0B0' --header 'Cookie: csrftoken=xZqBrKBPBOUANsWFnMC3aF90S52Ip3tgXdUHwWZvhNnu9aLl9j4rdrxRhV9nSQx9' --data $bodyData)
-    sleep 2
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Update Ad Compliance Review Status Completed" >> "$logfile"
 else
-    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - New Review Status NOT Supported {$reviewStatus}" >> "$logfile"
+    echo "$(date +%Y/%m/%d_%H:%M:%S) - (reviewStatus) - [$itemId] - Item's Metadata Main Group NOT Supported {$itemMetadataMainGroup}" >> "$logfile"
 fi
 
 IFS=$saveIFS
