003dbe1fd4314b1ba9aecd35bec4beea4417663d
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2024-10-17 10:38:27 -0400

    email fingerprint results

--- metadataFunctions/aiEmailResult.py
+++ metadataFunctions/aiEmailResult.py
@@ -20,13 +20,36 @@ import xml.etree.ElementTree as ET
 import requests
 import json
 import smtplib
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 from requests.exceptions import HTTPError
 #------------------------------
 
 #------------------------------
 # Internal Functions
-#------------------------------
+
+def readCantemoMetadata(rcmItemId, rcmFieldName):
+  #------------------------------
+  # Making API to Cantemo to get lookup values
+  headers = {
+    'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
+    'Cookie': 'csrftoken=OtjDQ4lhFt2wJjGaJhq3xi05z3uA6D8F7wCWNVXxMuJ8A9jw7Ri7ReqSNGLS2VRR',
+    'Accept': 'application/json'
+  }
+  payload = {}
+  urlGetMetadata = f"http://10.1.1.34:8080/API/item/{rcmItemId}/metadata?field={rcmFieldName}&terse=yes&interval=generic"
+  httpApiResponse = requests.request("GET", urlGetMetadata, headers=headers, data=payload)
+  httpApiResponse.raise_for_status()
+  #------------------------------
+  # Parsing JSON data
+  responseJson = httpApiResponse.json() if httpApiResponse and httpApiResponse.status_code == 200 else None
+  if responseJson and 'item' in responseJson:
+    for itemInformation in responseJson['item']:
+      for titleInformation in itemInformation[rcmFieldName]:
+        metadataValue = titleInformation['value']
+        if metadataValue == '':
+          metadataValue = '<none>'
+  #------------------------------
+  return metadataValue
 
 #------------------------------
 
@@ -41,19 +64,23 @@ try:
   smtpUsername = 'notify@olympusat.com'
   smtpPassword = '6bOblVsLg9bPQ8WG7JC7f8Zump'
   errorReport = ''
-  olderFileDayLimit = 14
+  olderFileDayLimit = 30
 
   if analysisType == "fingerprint":
     resultFile = f"/opt/olympusat/resources/vionlabsReports/{cantemoItemId}_FP.json"
+    resultFilename = f"{cantemoItemId}_FP.json"
   elif analysisType == "bingemarkers":
     resultFile = f"/opt/olympusat/resources/vionlabsReports/{cantemoItemId}_BM.json"
+    resultFilename = f"{cantemoItemId}_BM.json"
   elif analysisType == "adbreaks":
     resultFile = f"/opt/olympusat/resources/vionlabsReports/{cantemoItemId}_AB.json"
+    resultFilename = f"{cantemoItemId}_AB.json"
   
   if os.path.isfile(resultFile):
     resultFileCreation = os.path.getctime(resultFile)
     timeNow = time.time()
     fileCreationLimitOffset = timeNow - (60 * 60 * 24 * olderFileDayLimit)
+    
     if resultFileCreation > fileCreationLimitOffset:
       existingAnalysisRead = open(resultFile, "r")
       responseJson = json.loads(existingAnalysisRead.read())
@@ -66,12 +93,12 @@ try:
       }
       payload = {}
       if analysisType == "fingerprint":
-        urlGetVionlabResult = f"https://apis.prod.vionlabs.com/results/fingerprintplus/v1/{cantemoItemId}?&key={customerKey}"
+        urlGetVionlabsResult = f"https://apis.prod.vionlabs.com/results/fingerprintplus/v1/{cantemoItemId}?&key={customerKey}"
       elif analysisType == "bingemarkers":
-        urlGetVionlabResult = f"https://apis.prod.vionlabs.com/results/markers/v1/asset/{cantemoItemId}?&key={customerKey}"
+        urlGetVionlabsResult = f"https://apis.prod.vionlabs.com/results/markers/v1/asset/{cantemoItemId}?&key={customerKey}"
       elif analysisType == "adbreaks":
-        urlGetVionlabResult = f"https://apis.prod.vionlabs.com/results/adbreaks/v2/filter/frame/{cantemoItemId}?&key={customerKey}"
-      httpApiResponse = requests.request("GET", urlGetVionlabResult, headers=headers, data=payload)
+        urlGetVionlabsResult = f"https://apis.prod.vionlabs.com/results/adbreaks/v2/filter/frame/{cantemoItemId}?&key={customerKey}"
+      httpApiResponse = requests.request("GET", urlGetVionlabsResult, headers=headers, data=payload)
       httpApiResponse.raise_for_status()
       responseJson = httpApiResponse.json()
       #------------------------------
@@ -99,14 +126,30 @@ try:
     else:
       requesterEmail = f"{cantemoUsername}@olympusat.com"
 
-    msg = f"Attachment - result file for {analysisType} created on {resultFileCreation}"
-    msg['Subject'] = f"Result file for {analysisType}"
+    cantemoTitle = readCantemoMetadata(cantemoItemId, 'title')
+    time.sleep(1)
+    cantemoTitleCode = readCantemoMetadata(cantemoItemId, 'oly_titleCode')
+    # time.sleep(1)
+    # cantemoRightslineId = readCantemoMetadata(cantemoItemId, 'oly_rightslineItemId')
+
+    resultFileCreationStr = datetime.datetime.fromtimestamp(resultFileCreation).strftime('%m-%d-%Y %H:%M:%S')
+    msg = EmailMessage()
+    msg['Subject'] = f"MAM - Result file for {analysisType} analysis"
     msg['To'] = requesterEmail
-    msg['From'] = "cantemo@olympusat.com"
+    msg['From'] = "notify@olympusat.com"
+    msg.set_content(f"Attachment - result file for {analysisType} created on {resultFileCreationStr}\nTitle: {cantemoTitle}\nTitle Code: {cantemoTitleCode}")
+    with open(resultFile, 'rb') as f:
+      file_data = f.read()
+    msg.add_attachment(file_data, maintype='text', subtype='plain', filename=resultFilename)
 
     #------------------------------
     # Email file to initiating user
-    s = smtplib.SMTP(smtpHost)
+    s = smtplib.SMTP(smtpHost, 587)
+    s.connect(smtpHost, 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    # s.set_debuglevel(1)
     s.login(smtpUsername, smtpPassword)
     s.send_message(msg)
     s.quit()
@@ -114,7 +157,7 @@ try:
 
     #------------------------------
     # Update The User
-    print(f"{resultFile} (created on {resultFileCreation}) has been emailed to {requesterEmail}")
+    print(f"{resultFile} (created on {resultFileCreationStr}) has been emailed to {requesterEmail}")
     #------------------------------
     #------------------------------
 
