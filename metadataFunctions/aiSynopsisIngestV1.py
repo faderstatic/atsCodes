999a3808a025bf2c95e38a9838b3b3cacf26cc0b
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2025-01-10 17:15:27 -0500

    synopsis v1

--- metadataFunctions/aiSynopsisIngestV1.py
+++ metadataFunctions/aiSynopsisIngestV1.py
@@ -2,9 +2,9 @@
 # /usr/local/bin/python3
 #!/usr/bin/python3
 
-# This application ingests metadata from an XML API response into Cantemo
+# This application ingests metadata from an API response into Cantemo
 # PREREQUISITE: -none-
-# 	Usage: aiFingerprintIngestV1.py [Cantemo ItemID]
+# 	Usage: aiSynopsisIngestV1.py [Cantemo ItemID]
 
 #------------------------------
 # Libraries
@@ -24,162 +24,75 @@ from requests.exceptions import HTTPError
 
 #------------------------------
 # Internal Functions
-
-def readCantimoLookup(rclFieldName):
-  #------------------------------
-  # Making API to Cantemo to get lookup values
-  headers = {
-    'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
-    'Cookie': 'csrftoken=OtjDQ4lhFt2wJjGaJhq3xi05z3uA6D8F7wCWNVXxMuJ8A9jw7Ri7ReqSNGLS2VRR',
-    'Accept': 'application/json'
-  }
-  urlGetLookup = f"http://10.1.1.34:8080/API/metadata-field/{rclFieldName}/metadata"
-  payload = {}
-  httpApiResponse = requests.request("GET", urlGetLookup, headers=headers, data=payload)
-  httpApiResponse.raise_for_status()
-  #------------------------------
-
-  #------------------------------
-  # Parsing JSON data for lookup values
-  responseJson = httpApiResponse.json() if httpApiResponse and httpApiResponse.status_code == 200 else None
-  if responseJson and 'field' in responseJson:
-    for fieldInformation in responseJson['field']:
-      if fieldInformation['key'] == "__values":
-        lookupValueXml = fieldInformation['value']
-        # print(lookupValueXml)
-  #------------------------------
-
-  #------------------------------
-  # Parsing XML data
-  ET.register_namespace('ns', 'http://xml.vidispine.com/schema/vidispine')
-  lookupXmlRoot = ET.fromstring(lookupValueXml)
-  lookupList = []
-  # print(lookupXmlRoot.find('{http://xml.vidispine.com/schema/vidispine}field'))
-  for fieldValue in lookupXmlRoot:
-    lookupValue = fieldValue.find('{http://xml.vidispine.com/schema/vidispine}key')
-    # lookupList += lookupValue.text.lower()+','
-    lookupList.append(lookupValue.text.lower())
-  # print(lookupList)
-  #------------------------------
-  return lookupList
-
-def createCantimoLookup(cckFieldName, cckLookupXMLPayload):
-  headers = {
-  'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
-  'Cookie': 'csrftoken=HFOqrbk9cGt3qnc6WBIxWPjvCFX0udBdbJnzCv9jECumOjfyG7SS2lgVbFcaHBCc',
-  'Content-Type': 'application/xml'
-  }
-  urlPutLookupValues = f"http://10.1.1.34:8080/API/metadata-field/{cckFieldName}/values"
-  httpApiResponse = requests.request("PUT", urlPutLookupValues, headers=headers, data=cckLookupXMLPayload)
-  pass
-
 #------------------------------
 
 #------------------------------
 
 try:
   olderFileDayLimit = 14
-
   cantemoItemId = sys.argv[1]
   # cantemoItemId = os.environ.get("portal_itemId")
   errorReport = ''
-  outputFPFile = f"/opt/olympusat/resources/vionlabsReports/{cantemoItemId}_FP.json"
+  outputSynopsisFile = f"/opt/olympusat/resources/vionlabsReports/{cantemoItemId}_SN.json"
   
-  if not os.path.isfile(outputFPFile):
+  if not os.path.isfile(outputSynopsisFile):
     #------------------------------
-    # Making API call to Vionlabs to get fingerprints
+    # Making API call to Vionlabs to get synopsis
     headers = {
       'Accept': 'application/json'
     }
     payload = {}
-    urlGetFingerprintPlus = f"https://apis.prod.vionlabs.com/results/fingerprintplus/v1/{cantemoItemId}?&key=kt8cyimHXxUzFNGyhd7c7g"
-    # urlGetProfanitySegments = f"https://apis.prod.vionlabs.com/results/fingerprintplus/v1/OLT-003?&key=kt8cyimHXxUzFNGyhd7c7g"
-    httpApiResponse = requests.request("GET", urlGetFingerprintPlus, headers=headers, data=payload)
+    urlGetSynopsis = f"https://apis.prod.vionlabs.com/results/synopsis/v1/{cantemoItemId}?version=1&key=kt8cyimHXxUzFNGyhd7c7g"
+    httpApiResponse = requests.request("GET", urlGetSynopsis, headers=headers, data=payload)
     httpApiResponse.raise_for_status()
     responseJson = httpApiResponse.json()
     #------------------------------
     # Writing response data to a file
     apiResponseJson = json.loads(httpApiResponse.text)
     apiResponseJsonFormat = json.dumps(apiResponseJson, indent=2)
-    responseWriting = open(outputFPFile, "w")
+    responseWriting = open(outputSynopsisFile, "w")
     responseWriting.write(apiResponseJsonFormat)
     responseWriting.close()
     #------------------------------
   else:
-    outputFPFileCreation = os.path.getctime(outputFPFile)
+    outputSNFileCreation = os.path.getctime(outputSynopsisFile)
     timeNow = time.time()
     fileCreationLimitOffset = timeNow - (60 * 60 * 24 * olderFileDayLimit)
-    if outputFPFileCreation > fileCreationLimitOffset:
-      existingAnalysisRead = open(outputFPFile, "r")
+    if outputSNFileCreation > fileCreationLimitOffset:
+      existingAnalysisRead = open(outputSynopsisFile, "r")
       responseJson = json.loads(existingAnalysisRead.read())
       existingAnalysisRead.close()
     else:
       #------------------------------
-      # Making API call to Vionlabs to get fingerprints
+      # Making API call to Vionlabs to get synopsis
       headers = {
         'Accept': 'application/json'
       }
       payload = {}
-      urlGetFingerprintPlus = f"https://apis.prod.vionlabs.com/results/fingerprintplus/v1/{cantemoItemId}?&key=kt8cyimHXxUzFNGyhd7c7g"
-      # urlGetProfanitySegments = f"https://apis.prod.vionlabs.com/results/fingerprintplus/v1/OLT-003?&key=kt8cyimHXxUzFNGyhd7c7g"
-      httpApiResponse = requests.request("GET", urlGetFingerprintPlus, headers=headers, data=payload)
+      urlGetSynopsis = f"https://apis.prod.vionlabs.com/results/synopsis/v1/{cantemoItemId}?version=1&key=kt8cyimHXxUzFNGyhd7c7g"
+      httpApiResponse = requests.request("GET", urlGetSynopsis, headers=headers, data=payload)
       httpApiResponse.raise_for_status()
       responseJson = httpApiResponse.json()
       #------------------------------
       # Writing response data to a file
       apiResponseJson = json.loads(httpApiResponse.text)
       apiResponseJsonFormat = json.dumps(apiResponseJson, indent=2)
-      responseWriting = open(outputFPFile, "w")
+      responseWriting = open(outputSynopsisFile, "w")
       responseWriting.write(apiResponseJsonFormat)
       responseWriting.close()
       #------------------------------
 
-  addingGenreLookup = "false"
-  genreLookupXML = f"<SimpleMetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\">"
-  genreXML = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><group>Olympusat</group><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_genreAnalysis</name>"
-  moodXML = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><group>Olympusat</group><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_moodAnalysis</name>"
-  keywordXML = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><group>Olympusat</group><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_keywordAnalysis</name>"
   #------------------------------
-  # Parsing JSON and POST XML data
+  # Parsing JSON and post XML data
+  responseData = responseJson["data"]
+  synopsisType = responseData["synopsis"]
+  synopsisShort = synopsisType["synopsis_short"]
+  synopsisLong = synopsisType["synopsis_long"]
+  overviewShort = synopsisType["overview_with_vibe_short"]
+  overviewLong = synopsisType["overview_with_vibe_long"]
+  # print(f"ss: {synopsisShort}\nsl: {synopsisLong}\nos: {overviewShort}\nol: {overviewLong}")
 
-  genreList = readCantimoLookup("oly_genreAnalysis")
-  for individualGenre in genreList:
-    genreLookupXML += f"<field><key>{individualGenre}</key><value>{individualGenre}</value></field>"
-  for individualGenre in responseJson["genre"]:
-    genreXML += f"<value>{individualGenre}</value>"
-    if individualGenre.lower() not in genreList:
-      addingGenreLookup = "true"
-      genreLookupXML += f"<field><key>{individualGenre}</key><value>{individualGenre}</value></field>"
-  genreLookupXML += "</SimpleMetadataDocument>"
-  if addingGenreLookup == 'true':
-    parsedGenreLookupXML = xml.dom.minidom.parseString(genreLookupXML)
-    genreLookupPayload = parsedGenreLookupXML.toprettyxml()
-    createCantimoLookup("oly_genreAnalysis", genreLookupPayload)
-  genreXML += "</field></timespan></MetadataDocument>"
-  parsedGenreXML = xml.dom.minidom.parseString(genreXML)
-  genrePayload = parsedGenreXML.toprettyxml()
-
-  for individualMood in responseJson["mood_tag"]:
-    itemValue = responseJson["mood_tag"][individualMood]
-    itemValue *= 100
-    itemValue = round(itemValue, 2)
-    moodTag = str(itemValue) + f": {individualMood}"
-    moodXML += f"<value>{moodTag}</value>"
-  moodXML += "</field></timespan></MetadataDocument>"
-  parsedMoodXML = xml.dom.minidom.parseString(moodXML)
-  moodPayload = parsedMoodXML.toprettyxml()
-
-  for individualKeyword in responseJson["keyword"]:
-    itemValue = responseJson["keyword"][individualKeyword]
-    itemValue *= 100
-    itemValue = round(itemValue, 2)
-    keywordTag = str(itemValue) + f": {individualKeyword}"
-    keywordXML += f"<value>{keywordTag}</value>"
-  keywordXML += "</field></timespan></MetadataDocument>"
-  parsedKeywordXML = xml.dom.minidom.parseString(keywordXML)
-  keywordPayload = parsedKeywordXML.toprettyxml()
-
-  statusRawPayload = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_analysisStatus</name><value>completed - last request - fingerprint</value></field></timespan></MetadataDocument>"
+  statusRawPayload = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_analysisStatus</name><value>completed - last request - synopsis</value></field></timespan></MetadataDocument>"
   parsedStatusPayload = xml.dom.minidom.parseString(statusRawPayload)
   statusPayload = parsedStatusPayload.toprettyxml()
 
@@ -191,14 +104,18 @@ try:
   'Content-Type': 'application/xml'
   }
   urlPutAnalysisInfo = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata/"
-  # genrePayload = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_analysisReport</name><value>{errorReport}</value></field></timespan></MetadataDocument>"
-  httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=genrePayload)
-  time.sleep(5)
-  httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=moodPayload)
-  time.sleep(5)
-  httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=keywordPayload)
-  time.sleep(5)
-  httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=statusPayload)
+  metadataRawPayload = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\">"\
+  "<field><name>oly_aiSynopsisShort</name><value>{synopsisShort}</value>"\
+  "</field><field><name>oly_aiSynopsisLong</name><value>{synopsisLong}</value></field>"\
+  "<field><name>oly_aiOverviewShort</name><value>{overviewShort}</value></field>"\
+  "<field><name>oly_aiOverviewLong</name><value>{overviewLong}</value></field>"\
+  "<field><name>oly_analysisStatus</name><value>completed - last request - synopsis</value></field>"\
+  "</timespan></MetadataDocument>"
+  parsedMetadataPayload = xml.dom.minidom.parseString(metadataRawPayload)
+  metadataPayload = parsedMetadataPayload.toprettyxml()
+  print(metadataPayload)
+  httpApiResponse = requests.request("PUT", urlPutAnalysisInfo, headers=headers, data=metadataPayload)
+  httpApiResponse.raise_for_status()
   #------------------------------
   #------------------------------
 
