f89651ca994f83bbad67b78c657e370ef9732dcd
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2025-02-26 18:47:12 -0500

    adding charset

--- metadataFunctions/mdbGetInfoV2.py
+++ metadataFunctions/mdbGetInfoV2.py
@@ -139,8 +139,7 @@ try:
   headers = {
   'Authorization': 'Basic YWRtaW46MTBsbXBAc0B0',
   'Cookie': 'csrftoken=HFOqrbk9cGt3qnc6WBIxWPjvCFX0udBdbJnzCv9jECumOjfyG7SS2lgVbFcaHBCc',
-  'Content-Type': 'application/xml',
-  'charset': 'utf-8'
+  'Content-Type': 'application/xml charset=utf-8'
   }
   urlPutAnalysisInfo = f"http://10.1.1.34:8080/API/item/{cantemoItemId}/metadata/"
   itemIdRawPayload = f"<MetadataDocument xmlns=\"http://xml.vidispine.com/schema/vidispine\"><timespan start=\"-INF\" end=\"+INF\"><field><name>oly_omdbCombinedResult</name><value>{omdbCombinedResult}</value></field><field><name>oly_tmdbCombinedResult</name><value>{tmdbCombinedResult}</value></field></timespan></MetadataDocument>"
