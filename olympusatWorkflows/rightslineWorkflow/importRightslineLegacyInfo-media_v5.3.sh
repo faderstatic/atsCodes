1933824c5031910672202d30b7638002fc2609e4
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2025-03-20 20:46:58 -0400

    adding a whole bunch of metadata

--- olympusatWorkflows/rightslineWorkflow/importRightslineLegacyInfo-media_v5.3.sh
+++ olympusatWorkflows/rightslineWorkflow/importRightslineLegacyInfo-media_v5.3.sh
@@ -266,6 +266,171 @@ then
                     columnCounter=$(($columnCounter + 1))
                 fi
             ;;
+            "oly_contentType")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    contentType="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_contentType - [$contentType]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_contentType</name>\n         <value>$contentType</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_titleEn")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    titleEn="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_titleEn - [$titleEn]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_titleEn</name>\n         <value>$titleEn</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_titleEs")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    titleEs="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_titleEs - [$titleEs]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_titleEs</name>\n         <value>$titleEs</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_originalTitle")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    originalTitle="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_originalTitle - [$originalTitle]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_originalTitle</name>\n         <value>$originalTitle</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_descriptionEn")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    descriptionEn="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_descriptionEn - [$descriptionEn]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_descriptionEn</name>\n         <value>$descriptionEn</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_shortDescriptionEn")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    shortDescriptionEn="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_shortDescriptionEn - [$shortDescriptionEn]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_shortDescriptionEn</name>\n         <value>$shortDescriptionEn</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_descriptionEs")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    descriptionEs="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_descriptionEs - [$descriptionEs]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_descriptionEs</name>\n         <value>$descriptionEs</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_shortDescriptionEs")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    shortDescriptionEs="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_shortDescriptionEs - [$shortDescriptionEs]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_shortDescriptionEs</name>\n         <value>$shortDescriptionEs</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_originalLanguage")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    originalLanguage="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_originalLanguage - [$originalLanguage]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_originalLanguage</name>\n         <value>$originalLanguage</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_countryOfOriginGenres")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    countryOfOriginGenres="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_countryOfOriginGenres - [$countryOfOriginGenres]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_countryOfOriginGenres</name>\n         <value>$countryOfOriginGenres</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_productionYear")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    productionYear="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_productionYear - [$productionYear]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_productionYear</name>\n         <value>$productionYear</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_cast")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    cast="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_cast - [$cast]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_cast</name>\n         <value>$cast</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_director")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    director="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_director - [$director]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_director</name>\n         <value>$director</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_producer")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    producer="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_producer - [$producer]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_producer</name>\n         <value>$producer</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
+            "oly_productionCompany")
+                if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
+                then
+                    productionCompany="${fieldValue[$columnCounter]}"
+                    echo "$(date +%Y/%m/%d_%H:%M:%S) - (importLegacyMetadata) - [$cantemoItemId] - oly_productionCompany - [$productionCompany]" >> "$logfile"
+                    apiPayload="$apiPayload       <field>\n           <name>oly_productionCompany</name>\n         <value>$productionCompany</value>\n      </field>\n"
+                    columnCounter=$(($columnCounter + 1))
+                else
+                    columnCounter=$(($columnCounter + 1))
+                fi
+            ;;
             "oly_episodeNumber")
                 if [[ ! -z "${fieldValue[$columnCounter]}" && "$bulkMetadataHttpResponse" != *"</${fieldName[$columnCounter]}>"* ]];
                 then
