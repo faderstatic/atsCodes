cde9eed0f898a6437619847f680f11c88c77e785
K Kanjanapitak <63143052+faderstatic@users.noreply.github.com>
2025-03-21 08:31:51 -0400

    catching tripple quotes

--- olympusatWorkflows/rightslineWorkflow/importRightslineLegacyInfo-media_v5.3_print.sh
+++ olympusatWorkflows/rightslineWorkflow/importRightslineLegacyInfo-media_v5.3_print.sh
@@ -181,8 +181,9 @@ if [ ! -z "$matchedRowNumber" ];
 then
     line=$(sed -n ''$matchedRowNumber'p' "$inputFile")
     echo "Line read - $line"
-    cleanLine=$(echo $line | sed -e 's/\"\"/-/g')
+    cleanLine=$(echo "$line" | sed -e 's/\"\"/-/g')
     cleanLine=$(echo "$cleanLine" | sed -e 's/&/\&amp;/g')
+    cleanLine=$(echo $cleanLine | sed -e 's/-\"/-/g')
     echo "Clean line read - $cleanLine"
     # columnsForThisRow=$(echo "$cleanLine" | awk 'BEGIN { FPAT = "([^,]*)|(\"[^\"]+)|(\"[^\"]+\")" } {print NF+1}' )
     columnsForThisRow=$(countDaMuthaFukkingColumns "$cleanLine")
